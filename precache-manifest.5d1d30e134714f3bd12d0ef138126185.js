d0afbd3abafe45f57eca4493a53c71285c5d92c8
unknown <leebabou@gmail.com>
2020-08-05 11:48:22 +0300

    Updates

---
+++ precache-manifest.5d1d30e134714f3bd12d0ef138126185.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01f267029f0ff1144c278111fcfb8c7",
+    "url": "/DayasDating/index.html"
+  },
+  {
+    "revision": "2c2385312af832421ece",
+    "url": "/DayasDating/static/css/2.006c5c95.chunk.css"
+  },
+  {
+    "revision": "91826d126e00240ecb9d",
+    "url": "/DayasDating/static/css/main.92fb1b5a.chunk.css"
+  },
+  {
+    "revision": "2c2385312af832421ece",
+    "url": "/DayasDating/static/js/2.35eaf4a5.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/DayasDating/static/js/2.35eaf4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91826d126e00240ecb9d",
+    "url": "/DayasDating/static/js/main.e11c14f1.chunk.js"
+  },
+  {
+    "revision": "d4c705b6849bed57be9a",
+    "url": "/DayasDating/static/js/runtime-main.054751e2.js"
+  },
+  {
+    "revision": "9603a66b45969fb74cd5c2a142933666",
+    "url": "/DayasDating/static/media/02.9603a66b.jpg"
+  },
+  {
+    "revision": "399620fa6ec5b9a258c28179e07e6bee",
+    "url": "/DayasDating/static/media/04.399620fa.jpg"
+  },
+  {
+    "revision": "786ecedb477eac24f3432365ad7748c3",
+    "url": "/DayasDating/static/media/05.786ecedb.jpg"
+  },
+  {
+    "revision": "ceb9c79e05248c5122234ce30fc4bc3e",
+    "url": "/DayasDating/static/media/avatar.ceb9c79e.jpg"
+  },
+  {
+    "revision": "036629dbea48c974a9eb3c85dcd26940",
+    "url": "/DayasDating/static/media/dayas.036629db.jpg"
+  },
+  {
+    "revision": "6e9b47df938853a616e216a5a4cb2346",
+    "url": "/DayasDating/static/media/dayas2.6e9b47df.jpg"
+  },
+  {
+    "revision": "4249b1bf2a9d534c570c8bb81def15f7",
+    "url": "/DayasDating/static/media/dayas3.4249b1bf.jpg"
+  },
+  {
+    "revision": "40795f3bd9504b4a03cea29df0d2b252",
+    "url": "/DayasDating/static/media/dayas4.40795f3b.jpg"
+  },
+  {
+    "revision": "6109817826e6253c18fc982501cbfb5e",
+    "url": "/DayasDating/static/media/grace1.61098178.jpg"
+  },
+  {
+    "revision": "b54e2c21f3d3a881ea6d5df9a3b0567f",
+    "url": "/DayasDating/static/media/grace2.b54e2c21.jpg"
+  },
+  {
+    "revision": "812ffefeea3cbb1f9822c26eed1ed78e",
+    "url": "/DayasDating/static/media/grage.812ffefe.JPG"
+  },
+  {
+    "revision": "cb8c2a85a0c56d8c867a0b351e1bd107",
+    "url": "/DayasDating/static/media/open.cb8c2a85.jpg"
+  }
+]);
